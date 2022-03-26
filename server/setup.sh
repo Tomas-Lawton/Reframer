cdf7fa5800483e26eb099620de8e5f2a5128ed9c
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-03-26 17:46:56 +1300

    idk

--- server/setup.sh
+++ server/setup.sh
@@ -1,8 +1,9 @@
 # install packages using both conda and pip, but just run conda environment
+conda activate
+conda install --file requirements.txt
+
 source env/bin/activate
 pip install -r requirements.txt
 deactivate
-conda activate
-conda install --file requirements.txt
 # start application server
 uvicorn main:app --reload
