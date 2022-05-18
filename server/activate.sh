00fbd1d49a81a61424c0a15cf5a088ad60671c7b
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-05-19 00:38:53 +1000

    improve setup stage

--- server/activate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#after setup, just use conda env without pip
-deactivate
-CONDA_BASE=$(conda info --base)
-source $CONDA_BASE/etc/profile.d/conda.sh
-conda activate
-uvicorn main:app --reload
-
--- server/setup.sh
+++ server/setup.sh
@@ -2,10 +2,26 @@
 conda update -n base -c defaults conda
 conda create -n conda_env python=3.8 -y -y
 conda activate conda_env
+
+rm -rf diffvg
+rm -rf results
+rm -rf tmp
+
 conda install --file requirements.txt
-python3 -m venv env
-source env/bin/activate
-pip install -r requirements.txt
+pip install -r pip_requirements.txt
+
+git clone https://github.com/BachiLi/diffvg.git
+cp fix_problem.py diffvg/fix_problem.py
+cd diffvg
+git submodule update --init --recursive
+python3 fix_problem.py
+python setup.py install
+
+cd ..
 mkdir results
 mkdir tmp
-source ./activate.sh
+
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate
+uvicorn main:app --reload
