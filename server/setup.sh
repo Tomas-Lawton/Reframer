def06318ed9b323799cb2dcca52c59ca8f051cc0
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-05-19 22:10:50 +1000

    add diffvg for now

--- server/setup.sh
+++ server/setup.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# conda update -n base -c defaults conda
-# conda create -n conda_env python=3.8 -y -y
-# conda create -n conda_env python=3.8.13 -y -y
-# conda activate conda_env
-# conda install --file setup/conda-requirements.txt
-# pip install -r setup/pip_requirements.txt
+# rm -rf diffvg
+# rm -rf src/results
+# rm -rf src/tmp
+# mkdir src/results
+# mkdir src/tmp
 
-cd src
-rm -rf src/diffvg
-rm -rf src/results
-rm -rf src/tmp
-mkdir src/results
-mkdir src/tmp
 
-git clone https://github.com/BachiLi/diffvg.git
-cp fix_problem.py src/diffvg/fix_problem.py
-cd diffvg
-git submodule update --init --recursive
-python3 fix_problem.py
-python setup.py install
-
-echo "Setup Completed"
+# DO THIS MANUALLY
+# git clone https://github.com/BachiLi/diffvg.git
+# cp fix.py diffvg/fix.py
+# cd diffvg
+# git submodule update --init --recursive
+# python3 fix.py
+# python3 src/diffvg/setup.py install
