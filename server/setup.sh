fe71e88a6b0e945173ab117b506824963937df31
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-05-19 19:26:10 +1000

    deploying

--- server/setup.sh
+++ server/setup.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-conda update -n base -c defaults conda
-conda create -n conda_env python=3.8 -y -y
-conda create -n conda_env python=3.8.13 -y -y
-conda activate conda_env
+# conda update -n base -c defaults conda
+# conda create -n conda_env python=3.8 -y -y
+# conda create -n conda_env python=3.8.13 -y -y
+# conda activate conda_env
+# conda install --file setup/conda-requirements.txt
+# pip install -r setup/pip_requirements.txt
 
-rm -rf diffvg
-rm -rf results
-rm -rf tmp
-
-conda install --file setup/conda-requirements.txt
-pip install -r setup/pip_requirements.txt
+cd app
+rm -rf app/diffvg
+rm -rf app/results
+rm -rf app/tmp
+mkdir app/results
+mkdir app/tmp
 
 git clone https://github.com/BachiLi/diffvg.git
-cp setup/fix_problem.py diffvg/fix_problem.py
+cp fix_problem.py app/diffvg/fix_problem.py
 cd diffvg
 git submodule update --init --recursive
 python3 fix_problem.py
 python setup.py install
 
-cd ..
-mkdir results
-mkdir tmp
-
-CONDA_BASE=$(conda info --base)
-source $CONDA_BASE/etc/profile.d/conda.sh
-conda activate
-uvicorn main:app
+echo "Setup Completed"
