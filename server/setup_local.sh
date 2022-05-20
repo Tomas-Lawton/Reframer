d2e0f7cce8aa078bd6ddca3bdac295bdaacf239e
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-05-20 11:43:43 +1000

    add local builder back

--- server/setup_local.sh
+++ server/setup_local.sh
@@ -2,16 +2,18 @@
 conda update -n base -c defaults conda
 conda create -n conda_env python=3.8.13 -y -y
 conda activate conda_env
-conda install --file conda-requirements.txt
-pip install -r pip_requirements.txt
+conda install --file conda_requirements.txt
+pip install -r requirements.txt
+pip install git+https://github.com/openai/CLIP.git
+pip install 'pymongo[srv]'
 
-cd src
 rm -rf src/diffvg
 rm -rf src/results
 rm -rf src/tmp
 mkdir src/results
 mkdir src/tmp
 
+cd src
 git clone https://github.com/BachiLi/diffvg.git
 cd diffvg
 git submodule update --init --recursive
@@ -19,3 +21,5 @@ cp ../fix.py fix.py
 python3 fix.py
 python setup.py install
 echo "Setup Complete"
+cd ..
+uvicorn main:app --reload  
