e0f28ec6e1988695f0615d73378ab9cad1dbb0d2
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-05-23 01:32:48 +1000

    idk why thread breaks

--- server/dev_setup.sh
+++ server/dev_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 conda update -n base -c defaults conda
 conda env create -f environment.yml
+conda create -f environment.yml python=3.8.13 -y -y
+
 conda activate conda_env
 pip install -r requirements.txt
 
@@ -15,9 +17,8 @@ git clone https://github.com/BachiLi/diffvg.git
 cp fix.py diffvg/fix.py
 cd diffvg
 git submodule update --init --recursive
-# don't use python3
-python fix.py
-python setup.py install
+python3 fix.py
+python3 setup.py install
 
 cd ..
 CONDA_BASE=$(conda info --base)
