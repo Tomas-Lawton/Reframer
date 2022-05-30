7551cae4b7491e7f9cbf7fef9a501894ba1e8fc4
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-05-31 01:12:23 +1000

    drawer

--- server/src/main.py
+++ server/src/main.py
@@ -21,7 +21,7 @@ origins = [
     "http://localhost",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5500",
-    "https://tomas-lawton.github.io/AIDraw",
+    "https://tomas-lawton.github.io/drawing-client",
     "http://localhost:5500"
 ]
 
