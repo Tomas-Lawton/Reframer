9d9ea8b921bb143685229e866e848780f5080060
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-06-30 01:33:01 +1000

    finished adding

--- server/src/drawer.py
+++ server/src/drawer.py
@@ -3,9 +3,8 @@ import torch
 import pydiffvg
 from util.processing import get_augment_trans
 from util.loss import CLIPConvLoss2
-from util.utils import area_mask, use_penalisation, k_max_elements
+from util.utils import area_mask, use_penalisation, k_min_elements
 from util.render_design import calculate_draw_region
-from util.render_design import add_shape_groups
 from util.clip_utility import get_noun_data, data_to_tensor
 
 import logging
@@ -91,7 +90,8 @@ class CICADA:
             points = [x0] + points_array
 
             if len(points) > 0:
-                path_list.append(data_to_tensor(path["color"], float(path['stroke_width'] * self.normaliseScaleFactor), points, num_segments, path["tie"])) #add tiepath["tie"]
+                path_list.append(data_to_tensor(path["color"], float(path['stroke_width'] * self.normaliseScaleFactor), 
+                    points, num_segments, path["tie"])) #add tiepath["tie"]
         return path_list
 
     def activate(self, add_curves):
@@ -152,36 +152,116 @@ class CICADA:
         img = img[:, :, :3].unsqueeze(0).permute(0, 3, 1, 2)  # NHWC -> NCHW
         return img
 
+
+    def run_epoch(self, t):
+        self.points_optim.zero_grad()
+        self.width_optim.zero_grad()
+        self.color_optim.zero_grad()
+
+        img = self.build_img(t)
+
+        img_loss = (
+            torch.norm((img - self.img0) * self.mask)
+            if self.w_img > 0
+            else torch.tensor(0, device=self.device)
+        )
+
+        self.img = img.cpu().permute(0, 2, 3, 1).squeeze(0)
+
+        loss = 0
+
+        img_augs = []
+        for n in range(num_augs):
+            img_augs.append(self.augment_trans(img))
+        im_batch = torch.cat(img_augs)
+        img_features = self.model.encode_image(im_batch)
+        for n in range(num_augs):
+            loss -= torch.cosine_similarity(
+                self.text_features, img_features[n : n + 1], dim=1
+            )
+            if self.use_neg_prompts:
+                loss += (
+                    -torch.cosine_similarity(
+                        self.negative_text_features, img_features[n : n + 1], dim=1 #allow multiple
+                    )
+                    * 0.3
+                )
+        self.img_features = img_features
+
+        points_loss = 0
+        widths_loss = 0
+        colors_loss = 0
+
+        for k in range(len(self.points_vars)):
+            if self.drawing.traces[k].is_fixed:
+                points_loss += torch.norm(self.points_vars[k] - self.points_vars0[k])
+                colors_loss += torch.norm(self.color_vars[k] - self.color_vars0[k])
+                widths_loss += torch.norm(
+                    self.stroke_width_vars[k] - self.stroke_width_vars0[k]
+                )
+
+        loss += self.w_points * points_loss
+        loss += self.w_colors * colors_loss
+        loss += self.w_widths * widths_loss
+        loss += self.w_img * img_loss
+
+        # geo_loss = self.clipConvLoss(img * self.mask + 1 - self.mask, self.img0)
+
+        # for l_name in geo_loss:
+        #     loss += w_geo * geo_loss[l_name]
+        # loss += args.w_geo * geo_loss['clip_conv_loss_layer3']
+
+        # Backpropagate the gradients.
+        loss.backward()
+
+        # Take a gradient descent step.
+        self.points_optim.step()
+        self.width_optim.step()
+        self.color_optim.step()
+        for trace in self.drawing.traces:
+            trace.shape.stroke_width.data.clamp_(1.0, max_width)
+            trace.shape_group.stroke_color.data.clamp_(0.0, 1.0)
+
+        self.losses = {
+            'global': loss,
+            # 'points': points_loss,
+            # 'widhts': widths_loss,
+            # 'colors': colors_loss,
+            # 'image': img_loss,
+            # 'geometric': geo_loss,
+        }
+
+    
     def prune(self):
         prune_ratio = self.prune_ratio
         with torch.no_grad():
 
             # Get points of tied traces
-            tied_points = []
-            for p, path in enumerate(self.path_list):
-                if path.is_fixed:
-                    tied_points += [
+            fixed_points = []
+            for trace in self.drawing.traces:
+                if trace.is_fixed:
+                    fixed_points += [
                         x.unsqueeze(0)
-                        for i, x in enumerate(self.shapes[p].points)
+                        for i, x in enumerate(trace.shape.points)
                         if i % 3 == 0
                     ]  # only points the path goes through
 
             # Compute distances
             dists = []
-            if tied_points:
-                tied_points = torch.cat(tied_points, 0)
-                for p, path in enumerate(self.path_list):
-                    if path.is_fixed:
-                        dists.append(-1000)
+            if fixed_points:
+                fixed_points = torch.cat(fixed_points, 0)
+                for trace in self.drawing.traces:
+                    if trace.is_fixed:
+                        dists.append(-1000)  # We don't remove fixed traces
                     else:
                         points = [
                             x.unsqueeze(0)
-                            for i, x in enumerate(self.shapes[p].points)
+                            for i, x in enumerate(trace.shape.points)
                             if i % 3 == 0
                         ]  # only points the path goes through
                         min_dists = []
                         for point in points:
-                            d = torch.norm(point - tied_points, dim=1)
+                            d = torch.norm(point - fixed_points, dim=1)
                             d = min(d)
                             min_dists.append(d.item())
 
@@ -189,16 +269,13 @@ class CICADA:
 
             # Compute losses
             losses = []
-            for p, path in enumerate(self.path_list):
-                if path.is_fixed:
-                    losses.append(1000)
+            for n, trace in enumerate(self.drawing.traces):
+                if trace.is_fixed:
+                    losses.append(1000)  # We don't remove fixed traces
                 else:
                     # Compute the loss if we take out the k-th path
-                    shapes = self.shapes[:p] + self.shapes[p + 1 :]
-                    shape_groups = add_shape_groups(
-                        self.shape_groups[:p], self.shape_groups[p + 1 :]
-                    )
-                    img = self.build_img(shapes, shape_groups, 5)
+                    shapes, shape_groups = self.drawing.all_shapes_but_kth(n)
+                    img = self.build_img(5, shapes, shape_groups)
                     img_augs = []
                     for n in range(self.num_augs):
                         img_augs.append(self.augment_trans(img))
@@ -215,120 +292,12 @@ class CICADA:
             scores = [-0.01 * dists[k] ** (0.5) + losses[k] for k in range(len(losses))]
 
             # Actual pruning
-            inds = k_max_elements(scores, int((1 - prune_ratio) * self.num_paths))
-
-            # Define the lists like this because using "for p in inds"
-            # may (and often will) change the order of the traces
-            self.shapes = [
-                self.shapes[p] for p in range(len(self.path_list)) if p in inds
-            ]
-            self.shape_groups = add_shape_groups(
-                [self.shape_groups[p] for p in range(len(self.path_list)) if p in inds],
-                [],
+            inds = k_min_elements(
+                scores, int(prune_ratio * len(self.drawing.traces))
             )
-            self.path_list = [
-                self.path_list[p] for p in range(len(self.path_list)) if p in inds
-            ]
+            self.drawing.remove_traces(inds)
 
         self.initialize_variables()
-        self.prune_ratio += self.p0 / len(self.prune_places)
-        logging.info("Prune complete")
-
-    def run_epoch(self):
-        t = self.iteration
-        logging.info(f"Starting run {t} in drawer {str(self.sketch_data_reference_index)}")
-
-        self.points_optim.zero_grad()
-        self.width_optim.zero_grad()
-        self.color_optim.zero_grad()
-
-        img = self.build_img(t)
-
-        img_loss = (
-            torch.norm((img - self.img0) * self.mask)
-            if self.w_img > 0
-            else torch.tensor(0, device=self.device)
-        )
-
-        self.img = img.permute(0, 2, 3, 1).squeeze(0)
-
-        loss = 0
-
-        img_augs = []
-        for n in range(self.num_augs):
-            img_augs.append(self.augment_trans(img))
-        im_batch = torch.cat(img_augs)
-        img_features = self.model.encode_image(im_batch)
-        for n in range(self.num_augs):
-            # loss -= torch.cosine_similarity(
-            #     self.text_features, img_features, dim=1
-            # )
-            # if self.use_neg_prompts:
-            #     loss += (
-            #         -torch.cosine_similarity(
-            #             self.negative_text_features, img_features, dim=1 #allow multiple
-            #         )
-            #         * 0.3
-            #     )
-
-            loss -= torch.cosine_similarity(
-                self.text_features, img_features[n : n + 1], dim=1
-            )
-            if self.use_neg_prompts:
-                loss += (
-                    -torch.cosine_similarity(
-                        self.negative_text_features, img_features[n : n + 1], dim=1 #allow multiple
-                    )
-                    * 0.3
-                )
-
-        self.img_features = img_features
-
-        points_loss = 0
-        widths_loss = 0
-        colors_loss = 0
-
-        for k in range(len(self.points_vars0)):
-            if self.path_list[k].is_fixed:
-                points_loss += torch.norm(self.points_vars[k] - self.points_vars0[k])
-                colors_loss += torch.norm(self.color_vars[k] - self.color_vars0[k])
-                widths_loss += torch.norm(
-                    self.stroke_width_vars[k] - self.stroke_width_vars0[k]
-                )
-
-        loss += self.w_points * points_loss
-        loss += self.w_colors * colors_loss
-        loss += self.w_widths * widths_loss
-        loss += self.w_img * img_loss
-        # geo_loss = self.clipConvLoss(img * self.mask + 1 - self.mask, self.img0)
-
-        # for l_name in geo_loss:
-        #     loss += self.w_geo * geo_loss[l_name]
-        # loss += self.w_geo * geo_loss['clip_conv_loss_layer3']
-
-        # Backpropagate the gradients.
-        loss.backward()
-
-        # Take a gradient descent step.
-        self.points_optim.step()
-        self.width_optim.step()
-        self.color_optim.step()
-        for path in self.shapes:
-            path.stroke_width.data.clamp_(1.0, self.max_width)
-        for group in self.shape_groups:
-            group.stroke_color.data.clamp_(0.0, 1.0)
-
-        self.losses = {
-            'global': loss,
-            'points': points_loss,
-            'widhts': widths_loss,
-            'colors': colors_loss,
-            # 'image': img_loss,
-            # 'geometric': geo_loss,
-        }
-
-        logging.info(f"Completed run {t} in drawer {str(self.sketch_data_reference_index)}")
-        self.iteration += 1
 
     async def render_client(self, t, loss, pruning=False):
         status = str(self.sketch_data_reference_index)
@@ -337,12 +306,16 @@ class CICADA:
         if self.sketch_data_reference_index is not None:
             self.resizeScaleFactor = 224 / self.frame_size
 
+
+        shapes = [trace.shape for trace in self.drawing.traces]
+        shape_groups = [trace.shape_group for trace in self.drawing.traces]
+
         pydiffvg.save_svg(
             f"results/output-{str(self.sketch_data_reference_index)}.svg",
             self.user_canvas_w,
             self.user_canvas_h,
-            self.shapes,
-            self.shape_groups,
+            shapes,
+            shape_groups,
         )
 
         svg = ""
@@ -386,6 +359,7 @@ class CICADA:
         if self.region['activate']:
             self.drawing_area = calculate_draw_region(self.region, self.normaliseScaleFactor)
 
+
     def continue_update_sketch(self, data):
         logging.info("Adding changes...")
         self.num_user_paths = int(data["data"]["num_user_paths"])
@@ -402,7 +376,8 @@ class CICADA:
     async def loop(self):
         while self.is_running and self.iteration < self.num_iter:
             try:
-                self.run_epoch()
+                self.run_epoch(self.iteration)
+                self.iteration += 1
                 # if self.device == "cpu":
                 await self.render_client(self.iteration, self.losses['global'])
                 # else: 
