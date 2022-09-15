89b1fefb39d70f99a99c38666ac6c83ddd6fc3fd
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2022-09-16 03:30:23 +1000

    stuff

--- web/scripts/setup/chart.js
+++ web/scripts/setup/chart.js
@@ -1,61 +1,54 @@
-const spark = new PaperScope();
-spark.setup(sparkCanvas);
-spark.activate();
+// const spark = new PaperScope();
+// spark.setup(sparkCanvas);
+// spark.activate();
 
-var rect = new Rectangle();
-rect.left = 100;
-rect.right = 200;
-rect.bottom = 400;
-rect.top = 200;
+// var rect = new Rectangle();
+// rect.left = 100;
+// rect.right = 200;
+// rect.bottom = 400;
+// rect.top = 200;
 
-let knob = new Path.Circle({
-    radius: 5,
-    fillColor: "#7B66FF",
-    strokeColor: "white",
-    strokeWidth: 3,
-    position: { x: spark.view.bounds.right, y: spark.view.bounds.centerY },
-});
+// let knob = new Path.Circle({
+//     radius: 5,
+//     fillColor: "#7B66FF",
+//     strokeColor: "white",
+//     strokeWidth: 3,
+//     position: { x: spark.view.bounds.right, y: spark.view.bounds.centerY },
+// });
 
-let lossLine = new Path({
-    position: { x: spark.view.bounds.right, y: spark.view.bounds.centerY },
-});
+// let lossLine = new Path({
+//     position: { x: spark.view.bounds.right, y: spark.view.bounds.centerY },
+// });
 
-var myPath = new Path({
-    strokeColor: "#7B66FF",
-    strokeWidth: 3,
-    // fillColor: "#7B66FF",
-});
+// var myPath = new Path({
+//     strokeColor: "#7B66FF",
+//     strokeWidth: 3,
+//     // fillColor: "#7B66FF",
+// });
 
-myPath.strokeColor = "black";
-// myPath.add(spark.view.bounds.bottomLeft);
-// for (var i = 1; i < points; i++) {
-//     var point = new Point(width / points * i, center.y);
-//     path.add(point);
-// }
-// myPath.add(spark.view.bounds.bottomRight);
+// myPath.strokeColor = "black";
 
-myPath.add(new Point(spark.view.bounds.right, spark.view.bounds.centerY));
+// myPath.add(new Point(spark.view.bounds.right, spark.view.bounds.centerY));
 
-let easing = 0.98;
+// let easing = 0.98;
 
-spark.view.onFrame = () => {
-    if (mainSketch.semanticLoss) {
-        let target = myPath.lastSegment.point.y; //ease?
-        let newY = scaleRange(mainSketch.semanticLoss, -1, 0, 0, 150);
-        let dy = target - newY;
-        newY += dy * easing;
+// spark.view.onFrame = () => {
+//     if (mainSketch.semanticLoss) {
+//         let target = myPath.lastSegment.point.y; //ease?
+//         let newY = scaleRange(mainSketch.semanticLoss, -1, 0, 0, 150);
+//         let dy = target - newY;
+//         newY += dy * easing;
 
-        let nextPoint = new Point(spark.view.bounds.right, newY);
-        myPath.add(nextPoint);
+//         let nextPoint = new Point(spark.view.bounds.right, newY);
+//         myPath.add(nextPoint);
 
-        sparkKnob.style.top = newY + "px";
+//         sparkKnob.style.top = newY + "px";
 
-        // let newY = new Point(spark.view.bounds.right, last + (rando < 0.5 ? 3 : -3));
-        knob.position.x -= 0.3;
-        myPath.position.x -= 0.3;
-    }
-    scope.activate();
-};
+//         knob.position.x -= 0.3;
+//         myPath.position.x -= 0.3;
+//     }
+//     scope.activate();
+// };
 
 // const ctx = sparkCanvas.getContext("2d");
 // const myChart = new Chart(ctx, {
